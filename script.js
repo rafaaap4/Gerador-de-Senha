769fd3a0123e318a1efb1233793624b7f030f0b3
rafaela <166190274+rafaaap4@users.noreply.github.com>
2024-06-14 23:16:05 -0300

    script.js

---
+++ script.js
@@ -0,0 +1,9 @@
+document.getElementById('generate').addEventListener('click', function() {
+    const length = 12;
+    const charset = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%^&*()_+";
+    let password = "";
+    for (let i = 0, n = charset.length; i < length; ++i) {
+        password += charset.charAt(Math.floor(Math.random() * n));
+    }
+    document.getElementById('password').value = password;
+});
